16f6b9c2ebf2cbf3dad43bd06aad47f4e96787a9
virustotalop <virustotalop@gmail.com>
2025-08-14 20:14:21 -0700

    Rework how index is constructor

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,14 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+import React from 'react';
+import { createRoot } from "react-dom/client";
 import TaskPage from "./pages/TaskPage";
 
-const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
-root.render(<TaskPage />);
+
+const container = document.getElementById("root");
+if (!container) throw new Error("Root container not found");
+
+const root = createRoot(container);
+root.render(
+  <React.StrictMode>
+    <TaskPage />
+  </React.StrictMode>
+);
